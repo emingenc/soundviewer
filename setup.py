6fc4d06fde295f4f476ce24596c521b8ba05405b
emingenc <emingenc@novit.ai>
2022-03-04 23:10:20 +0300

    update: readme remove --show

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
 
     # Start with a small number and increase it with every change you make
     # https://semver.org
-    version='1.0.2',
+    version='1.0.3',
 
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     # For example: GPL
